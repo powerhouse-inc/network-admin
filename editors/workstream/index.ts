7f57da75550ffa6e490209b6f6a4dbdac67f613b
liberuum <liberom@proton.me>
2025-10-02 15:38:08 +0100

    clean

--- editors/workstream/index.ts
+++ editors/workstream/index.ts
@@ -1,6 +1,5 @@
 import type { EditorModule } from "document-model";
 import Editor from "./editor.js";
-import type { WorkstreamDocument } from "../../document-models/workstream/index.js";
 
 export const module: EditorModule = {
   Component: Editor,
