5e1efec76d567f170ac75deb9d2bcffad1167986
liberuum <liberom@proton.me>
2025-11-05 15:03:30 +0000

    add allNetworks query

--- subgraphs/index.ts
+++ subgraphs/index.ts
@@ -1,6 +1,7 @@
-export { };
+export {};
 export * as NetworkProfileSubgraph from "./network-profile/index.js";
 export * as PaymentTermsSubgraph from "./payment-terms/index.js";
 export * as RequestForProposalsSubgraph from "./request-for-proposals/index.js";
 export * as WorkstreamSubgraph from "./workstream/index.js";
 export * as WorkstreamsSubgraph from "./workstreams/index.js";
+export * as NetworksSubgraph from "./networks/index.js";
